60619cb23b73b3f20f5c5d7aae5cf8867212c19d
Ahmed2120 <ahmed202ashraf202@gmail.com>
2023-09-23 15:56:07 +0300

    fix api key

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
+
 class AppConstants{
-  static String gptKey = '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';
+  static String gptKey = Platform.environment['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']!;
   static String organizationId = 'org-URfp0B70A5B4acunk60IXKay';
 }
